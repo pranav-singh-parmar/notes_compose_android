d148f0fbad512f9e93873ece00abd3046ff76b53
pranav-singh-parmar <71075740+pranav-singh-parmar@users.noreply.github.com>
2025-08-17 23:45:12 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activities/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                NotesApp()
+                NotesApp(notesViewModel = notesViewModel)
             }
         }
     }
